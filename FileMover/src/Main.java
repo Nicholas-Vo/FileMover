dbefbc167bdd3646cb87bb9f77547b8e8db82072
Harumb1 <97941428+Harumb1@users.noreply.github.com>
2025-02-10 09:30:42 +0200

    Update Main.java
    newPath method added, Scanner allows you to choose a new directory for the file to be moved in but when ran the program shows an error "stack overflow too deep".

--- FileMover/src/Main.java
+++ FileMover/src/Main.java
@@ -1,19 +1,28 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
-       String currentPath = new java.io.File("Movable.txt").getCanonicalPath();
-       System.out.println("Current dir: " + currentPath);
+    static Scanner sc = new Scanner(System.in);
+    static String currentPath;
+    static String newPath;
+    public static void main(String[] args) throws IOException {
+
+        currentPath = new java.io.File("Movable.txt").getCanonicalPath();
+        System.out.println("Current dir: " + currentPath);
+        newPath = String.valueOf(Path.of(sc.nextLine()));
+        moveFile(currentPath, newPath);
         getDirectory();
+        //moveFile(currentPath,newPath);
 
    }
    static void getDirectory(){
        try {
            PrintWriter file = new PrintWriter("Movable.txt");
-           file.append("This file will be moved with the magic of coding!!");
+           file.append("This file will be moved once it is moved to its new directory");
            /*Once the file is moved it will display a different message
-           "This file was moved with the magic of coding "
+           "This file was moved to a new directory "
             */
            file.close();
            System.out.println("File updated.");
@@ -23,4 +32,7 @@ public class Main {
 
        }
    }
+    private static void moveFile(String currentPath, String newPath) {
+        moveFile(currentPath, newPath);
+    }
 }
