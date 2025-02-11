21270c7ee07b3027f21ffa3cabc039555a27e74c
Harumb1 <97941428+Harumb1@users.noreply.github.com>
2025-02-11 12:12:16 +0200

    Update Main.java

--- FileMover/src/Main.java
+++ FileMover/src/Main.java
@@ -1,38 +1,40 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    static String currentPath;
-    static String newPath;
+    static Path currentPath;
+    static Path newPath;
+    static PrintWriter file;
     public static void main(String[] args) throws IOException {
 
-        currentPath = new java.io.File("Movable.txt").getCanonicalPath();
-        System.out.println("Current dir: " + currentPath);
-        newPath = String.valueOf(Path.of(sc.nextLine()));
-        moveFile(currentPath, newPath);
-        getDirectory();
-        //moveFile(currentPath,newPath);
+            currentPath = Paths.get("Movable.txt");
+            System.out.println("Current dir: " + "\n" + currentPath.toAbsolutePath());
+            System.out.println("New Path: ");
+            newPath = Paths.get(sc.nextLine());
 
-   }
-   static void getDirectory(){
-       try {
-           PrintWriter file = new PrintWriter("Movable.txt");
-           file.append("This file will be moved once it is moved to its new directory");
+            getDirectory();
+            moveFile();
+
+    }
+    static void getDirectory(){
+        try {
+            file = new PrintWriter("Movable.txt");
+            file.append("This file will be moved once it is moved to its new directory");
            /*Once the file is moved it will display a different message
            "This file was moved to a new directory "
             */
-           file.close();
-           System.out.println("File updated.");
-       } catch (IOException exception) {
-           System.out.println("Error!");
-           exception.printStackTrace();
+            file.close();
+            System.out.println("File updated.");
+        } catch (IOException exception) {
+            System.out.println("Error!");
+            exception.printStackTrace();
 
-       }
-   }
-    private static void moveFile(String currentPath, String newPath) {
-        moveFile(currentPath, newPath);
+        }
+    }
+    static void moveFile() throws IOException {
+        Files.move(currentPath,newPath);
     }
 }
